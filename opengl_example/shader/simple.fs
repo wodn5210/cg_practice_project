337673093c5dcb5f16c8ad888dd75aaf905f9f8f
wodn5210 <wodn5210@gmail.com>
2023-07-29 13:16:24 +0900

    Uniform 추가 주석

--- opengl_example/shader/simple.fs
+++ opengl_example/shader/simple.fs
@@ -2,6 +2,14 @@
 uniform vec4 color;
 out vec4 fragColor;
 
+/*
+    uniform 사용할때 context.cpp 에서 program instance 생성이후 아래 과정 해주면된다.
+    
+	auto loc = glGetUniformLocation(m_program->Get(), "color");
+    m_program->Use();
+    glUniform4f(loc, 1.0f, 1.0f, 0.0f, 1.0f);
+*/
+
 void main() {
     fragColor = color;
 }
